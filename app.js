f4f5175e493f0162c6913ba6b16eb8ba14cf1ef7
Nikos Vasil <nikos.vasileiadis91@gmail.com>
2025-03-05 15:39:03 +0100

    Daily traffic chart was added!

--- app.js
+++ app.js
@@ -57,3 +57,37 @@ let trafficChart = new Chart(trafficCanvas, {
     data: trafficData,
     options: trafficOptions
 });
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+// Data for daily traffic bar chart
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+// Options for daily traffic bar chart
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+// Create the bar chart
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
