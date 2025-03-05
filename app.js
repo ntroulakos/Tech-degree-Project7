be67dcf1ef40dbb0420cc3429c936b8dc1043e40
Nikos Vasil <nikos.vasileiadis91@gmail.com>
2025-03-05 10:07:16 +0100

    Alert banner was added!

---
+++ app.js
@@ -0,0 +1,14 @@
+const alertBanner = document.getElementById("alert");
+
+alertBanner.innerHTML = `
+    <div class="alert-banner">
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+        <p class="alert-banner-close">x</p>
+    </div>
+`;
+
+alertBanner.addEventListener('click', (e) => {
+    if (e.target.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none";
+    }
+});
